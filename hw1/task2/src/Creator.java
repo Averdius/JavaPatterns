b041f491455193e7961784eb48e8e5e54cf343a8
Averdius <62611821+Averdius@users.noreply.github.com>
2023-08-26 22:59:42 +0300

    Add files via upload

---
+++ hw1/task2/src/Creator.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+
+public class Creator {
+    public void someOperation() {
+        Product product = createProduct();
+        product.doSomething();
+    }
+
+    protected Product createProduct(){
+        Random random = new Random();
+        if(random.nextBoolean()){
+            return new Product1();
+        }else {
+            return  new Product2();
+        }
+    }
+}
